b2ef735fd52bb0f71e62e6cd79fba854d799d1c3
Flavien Scheurer <flavien+github@ystorian.com>
2022-10-19 12:56:59 +0200

    Change the .app names for the long form

--- build.sh
+++ build.sh
@@ -9,16 +9,16 @@ app_name="SVG to PNG"
 name=svg2png
 bundled_file="rsvg-convert|oxipng"
 
-/usr/local/bin/platypus --optimize-nib --overwrite --droppable --app-icon "$name.icns" --name "$name" --interface-type Droplet --interpreter "/bin/zsh" --bundle-identifier "$identifier"."$name" --uniform-type-identifiers public.svg-image --bundled-file "$bundled_file" --author "$author" --app-version $version "$name.sh" "$app_name"
+/usr/local/bin/platypus --optimize-nib --overwrite --droppable --app-icon "$name.icns" --name "$app_name" --interface-type Droplet --interpreter "/bin/zsh" --bundle-identifier "$identifier"."$name" --uniform-type-identifiers public.svg-image --bundled-file "$bundled_file" --author "$author" --app-version $version "$name.sh" "$app_name"
 
 app_name="SVG to ICNS"
 name=svg2icns
 bundled_file="rsvg-convert"
 
-/usr/local/bin/platypus --optimize-nib --overwrite --droppable --app-icon "$name.icns" --name "$name" --interface-type Droplet --interpreter "/bin/zsh" --bundle-identifier "$identifier"."$name" --uniform-type-identifiers public.svg-image --bundled-file "$bundled_file" --author "$author" --app-version $version "$name.sh" "$app_name"
+/usr/local/bin/platypus --optimize-nib --overwrite --droppable --app-icon "$name.icns" --name "$app_name" --interface-type Droplet --interpreter "/bin/zsh" --bundle-identifier "$identifier"."$name" --uniform-type-identifiers public.svg-image --bundled-file "$bundled_file" --author "$author" --app-version $version "$name.sh" "$app_name"
 
 app_name="SVG to folder icon"
 name=svg2folder
 bundled_file="rsvg-convert"
 
-/usr/local/bin/platypus --optimize-nib --overwrite --droppable --app-icon "$name.icns" --name "$name" --interface-type Droplet --interpreter "/bin/zsh" --bundle-identifier "$identifier"."$name" --uniform-type-identifiers public.svg-image --bundled-file "$bundled_file" --author "$author" --app-version $version "$name.sh" "$app_name"
+/usr/local/bin/platypus --optimize-nib --overwrite --droppable --app-icon "$name.icns" --name "$app_name" --interface-type Droplet --interpreter "/bin/zsh" --bundle-identifier "$identifier"."$name" --uniform-type-identifiers public.svg-image --bundled-file "$bundled_file" --author "$author" --app-version $version "$name.sh" "$app_name"
