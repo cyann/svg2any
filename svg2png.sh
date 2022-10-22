f230899e24e5f3dff96e0a3875c575f60863ed22
Flavien Scheurer <flavien+github@ystorian.com>
2022-10-22 23:41:48 +0200

    Reorder blocks

--- svg2png.sh
+++ svg2png.sh
@@ -7,51 +7,53 @@
 # and the faster but less efficient zlib algorithm for larger images.
 # Flavien Scheurer 2022
 
-# Set the app name and version from the script itself.
-app_name="$(basename "$0" .sh)"
-version="$(stat -f %Sm -t %Y%m%d )"
+# Declare variables.
+# App name and copyright information.
+app_name="svg2png"
+author="© 2022 Flavien Scheurer https://github.com/Ystorian/svg2any"
 
-# Reset the app name and version from the Platypus bundle if available.
-if [[ -v $__CFBundleIdentifier ]]; then
-	app_name="$__CFBundleIdentifier"
-	version="${defaults read $PWD/../Info CFBundleShortVersionString}"
-fi
+# App version, uses the script file date by default.
+version="$(stat -f %Sm -t %Y%m%d "$0")"
 
-# Other variables for this script.
+# Log file.
+log_file="$HOME/Library/Logs/$app_name.log"
+
+# Image size in pixels.
 size=1024
 input_ext=svg
 output_ext=png
 help="Convert a square SVG file to a $size ⨉ $size ${output_ext:u} file."
 repo=https://github.com/Ystorian/svg2any
 
-# Log file.
-log_file="$HOME/Library/Logs/$app_name.log"
+# Use the version from the bundle if available.
+if [[ -f "$PWD/../Info.plist" ]]; then
+	version="$(plutil -extract CFBundleShortVersionString raw "$PWD/../Info.plist")"
+fi
 
 # Prune the log file if larger than 1 MB.
-if [[ -f "$log_file" ]] ; then
+if [[ -f "$log_file" ]]; then
 	find "$log_file" -size +1M -delete
 fi
 
-# Log the app name, version, and path.
-echo "\n\n$(date +"%Y-%m-%d %H:%M:%S") $app_name $version" >> "$log_file"
-echo "$PWD" >> "$log_file"
-
-# Exit immediately if any command exits with a non-zero status.
-# set -e
-
 # Test if a file was selected.
-if [ "$1" = "" ] ; then
+if [[ "$1" = "" ]]; then
 	# No file selected, assuming we are running as a standalone script, display the help text.
-	echo "$app_name $version $repo"
-	echo $help
-	echo "The converted file will be {file}.$output_ext in the same directory."
-	echo "\n\tUsage: $app_name /{dir}/{file}.$input_ext"
-	echo "Error: File not specified" >> "$log_file"
+	echo "$app_name version $version"
+	echo "Convert an SVG file a PNG file."
+	echo "The converted file will be created in the same directory as the input file."
+	echo "\n\tUsage: $app_name file [size]"
 	exit
 fi
 
+# Log the app name, version, and path.
+echo "\n\n$(date +"%Y-%m-%d %H:%M:%S") $app_name version $version" >>"$log_file"
+echo "Running script: $0" >>"$log_file"
 
 # Define the file variables.
+
+# Supported file extensions.
+input_ext=svg
+output_ext=png
 
 # Base directory path.
 base_dir=$(dirname "$1")
@@ -66,20 +68,19 @@ base_file=${base_file%.*}
 # Output file name.
 output_file_name=$base_file.$output_ext
 
-
 # Test if the input file exists.
-if [[ ! -f "$base_dir/$input_file_name" ]] ; then
+if [[ ! -f "$base_dir/$input_file_name" ]]; then
 	echo "Error: File not found: $base_dir/$input_file_name" | tee -a "$log_file"
 	exit
 fi
 
-echo "Base directory: $base_dir" >> "$log_file"
-echo "Input file: $input_file_name" >> "$log_file"
-echo "Output file: $output_file_name" >> "$log_file"
+echo "Base directory: $base_dir" >>"$log_file"
+echo "Input file: $input_file_name" >>"$log_file"
+echo "Output file: $output_file_name" >>"$log_file"
 
 
 # Test if the output file already exists.
-if [[ -f "$base_dir/$output_file_name" ]] ; then
+if [[ -f "$base_dir/$output_file_name" ]]; then
 	echo "Overwriting existing output file." | tee -a "$log_file"
 	rm "$base_dir/$output_file_name"
 fi
@@ -101,5 +102,5 @@ echo "Compressing $output_file_name (using Zopfli - this may 2-3 minutes)..."
 ./oxipng --opt max --interlace 0 --strip safe --alpha -Z "$base_dir/$output_file_name" &>> "$log_file"
 
 # Display the path and size of the output file.
-echo "Created $base_dir/$output_file_name ($(stat -f %z "$base_dir/$output_file_name") bytes)"
-echo "$(date +"%Y-%m-%d %H:%M:%S") Done." >> "$log_file"
+echo "Created $base_dir/$output_file_name ($(stat -f %z "$base_dir/$output_file_name") bytes)" | tee -a "$log_file"
+echo "$(date +"%Y-%m-%d %H:%M:%S") Done." >>"$log_file"
