3740b7467c151202abb163b78b7bfcced35d7ac1
Flavien Scheurer <flavien+github@ystorian.com>
2022-10-25 17:36:59 +0200

    Cosmetic

--- build.sh
+++ build.sh
@@ -9,24 +9,38 @@ identifier=com.ystorian
 author="Flavien Scheurer"
 version=1.0.2
 
+##############
+# SVG to PNG #
+##############
 app_name="SVG to PNG"
 name="svg2png"
 
+# Build with Platypus.
 /usr/local/bin/platypus --optimize-nib --overwrite --droppable --app-icon "$name.icns" --name "$app_name" --interface-type Droplet --interpreter "/bin/zsh" --bundle-identifier "$identifier"."$name" --uniform-type-identifiers public.svg-image --bundled-file "Credits.html" --author "$author" --app-version $version "$name.sh" "$app_name"
 
 # Copy the required binaries in .app/Contents/MacOS.
 cp "rsvg-convert" "$app_name.app/Contents/MacOS"
 cp "oxipng" "$app_name.app/Contents/MacOS"
+
+###############
+# SVG to ICNS #
+###############
 app_name="SVG to ICNS"
 name="svg2icns"
 
+# Build with Platypus.
 /usr/local/bin/platypus --optimize-nib --overwrite --droppable --app-icon "$name.icns" --name "$app_name" --interface-type Droplet --interpreter "/bin/zsh" --bundle-identifier "$identifier"."$name" --uniform-type-identifiers public.svg-image --bundled-file "Credits.html" --author "$author" --app-version $version "$name.sh" "$app_name"
 
 # Copy the required binaries in .app/Contents/MacOS.
 cp "rsvg-convert" "$app_name.app/Contents/MacOS"
+
+######################
+# SVG to folder icon #
+######################
 app_name="SVG to folder icon"
 name="svg2folder"
 
+# Build with Platypus.
 /usr/local/bin/platypus --optimize-nib --overwrite --droppable --app-icon "$name.icns" --name "$app_name" --interface-type Droplet --interpreter "/bin/zsh" --bundle-identifier "$identifier"."$name" --uniform-type-identifiers public.svg-image --bundled-file "Credits.html" --author "$author" --app-version $version "$name.sh" "$app_name"
 
 # Copy the required binaries in .app/Contents/MacOS.
